25ba7b84acec5e4a014f39d85e66f31768fdb1bc
Tim Hall <tim.hall.engr@gmail.com>
2020-01-12 00:20:05 -0500

    Generate types

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' }
-  ],
-  plugins: [typescript()]
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' }
+    ],
+    plugins: [typescript()]
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: pkg.types, format: 'es' }],
+    plugins: [dts()]
+  }
+];
